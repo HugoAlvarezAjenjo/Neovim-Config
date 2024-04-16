47d3dd315c6d05a2566f4464fd353738f32d314e
Hugo Alvarez Ajenjo <hugo.alvarezajenjo@gmail.com>
2024-04-16 19:46:26 +0200

    Change telescope keymap

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -23,7 +23,7 @@ return {
 			local builtin = require("telescope.builtin")
 			vim.keymap.set("n", "<C-o>", builtin.find_files, {})
 			vim.keymap.set("n", "<C-p>", builtin.commands, {})
-			vim.keymap.set("n", "<leader>fg", builtin.live_grep, {})
+			vim.keymap.set("n", "<C-i>", builtin.live_grep, {})
 
 			require("telescope").load_extension("ui-select")
 		end,
