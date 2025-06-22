0785403fd8f3f2f12438ab9f8c6b4363c5fe9b81
Hugo Alvarez Ajenjo <hugo.alvarezajenjo@gmail.com>
2025-06-22 23:38:52 +0200

    Change keymap

--- lua/plugins/harpoon.lua
+++ lua/plugins/harpoon.lua
@@ -8,6 +8,6 @@ return {
         -- Set a vim motion to <Shift>m to mark a file with harpoon
         vim.keymap.set("n", "<s-m>", "<cmd>lua require('harpoon.mark').add_file()<cr>", {desc = "Harpoon Mark File"})
         -- Set a vim motion to the tab key to open the harpoon menu to easily navigate frequented files
-        vim.keymap.set("n", "<TAB>", "<cmd>lua require('harpoon.ui').toggle_quick_menu()<cr>", {desc = "Harpoon Toggle Menu"})
+        vim.keymap.set("n", "<leader><TAB>", "<cmd>lua require('harpoon.ui').toggle_quick_menu()<cr>", {desc = "Harpoon Toggle Menu"})
     end
 }
