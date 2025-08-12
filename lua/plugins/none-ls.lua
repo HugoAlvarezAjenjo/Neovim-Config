7f2336d1d3cf44025c116eff72029e94fd8a77ee
Hugo Alvarez Ajenjo <hugo.alvarezajenjo@gmail.com>
2025-08-12 21:59:08 +0200

    none ls

--- lua/plugins/none-ls.lua
+++ lua/plugins/none-ls.lua
@@ -1,45 +1,44 @@
 return {
-  "nvimtools/none-ls.nvim",
-  dependencies = {
-    "nvim-lua/plenary.nvim",
-  },
-  event = { "BufReadPost", "BufNewFile" },
-  config = function()
-    local null_ls = require("null-ls")
-    local formatting = null_ls.builtins.formatting
-    local diagnostics = null_ls.builtins.diagnostics
+	"nvimtools/none-ls.nvim",
+	dependencies = { "nvim-lua/plenary.nvim" },
+	event = { "BufReadPost", "BufNewFile" },
+	config = function()
+		local null_ls = require("null-ls")
 
-    -- List of desired sources (without availability checks)
-    local desired_sources = {
-      formatting.stylua,
-      formatting.prettier.with({
-        extra_args = { "--single-quote", "--jsx-single-quote" },
-      }),
-      formatting.google_java_format,
-      diagnostics.erb_lint,
-      diagnostics.rubocop,
-      formatting.rubocop,
-    }
+		-- Silent availability check (no notifications)
+		local function is_available(source)
+			local cmd = type(source.command) == "table" and source.command.command
+				or source._opts and source._opts.command
+				or source.command
+			return cmd and vim.fn.executable(cmd) == 1
+		end
 
-    -- Filter sources based on executable availability
-    local sources = {}
-    for _, source in ipairs(desired_sources) do
-      local command = source._opts and source._opts.command or source.command
-      if vim.fn.executable(command) == 1 then
-        table.insert(sources, source)
-      else
-        vim.notify_once("[null-ls] Not found: " .. command, vim.log.levels.WARN)
-      end
-    end
+		-- Sources with silent filtering
+		local sources = {}
+		for _, source in ipairs({
+			null_ls.builtins.formatting.stylua,
+			null_ls.builtins.formatting.prettier.with({
+				extra_args = { "--single-quote", "--jsx-single-quote" },
+			}),
+			null_ls.builtins.formatting.google_java_format,
+			null_ls.builtins.diagnostics.erb_lint,
+			null_ls.builtins.diagnostics.rubocop,
+			null_ls.builtins.formatting.rubocop,
+		}) do
+			if is_available(source) then
+				table.insert(sources, source)
+			end
+		end
 
-    null_ls.setup({
-      debug = false,
-      sources = sources,
-      on_attach = function(client)
-        if client.server_capabilities.documentFormattingProvider then
-          vim.keymap.set("n", "<leader>cf", vim.lsp.buf.format, { desc = "[C]ode [F]ormat" })
-        end
-      end,
-    })
-  end,
+		null_ls.setup({
+			sources = sources,
+			on_attach = function(client)
+				if client.server_capabilities.documentFormattingProvider then
+					vim.keymap.set("n", "<leader>cf", function()
+						vim.lsp.buf.format({ async = true })
+					end, { desc = "Format buffer" })
+				end
+			end,
+		})
+	end,
 }
