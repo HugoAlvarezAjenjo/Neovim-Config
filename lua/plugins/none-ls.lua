d813673ff391894556fb09eeb1d0bfd21398da2e
Hugo Alvarez Ajenjo <hugo.alvarezajenjo@gmail.com>
2025-08-12 21:55:00 +0200

    Some more

--- lua/plugins/none-ls.lua
+++ lua/plugins/none-ls.lua
@@ -1,16 +1,45 @@
 return {
-	"nvimtools/none-ls.nvim",
-	config = function()
-		local null_ls = require("null-ls")
-		null_ls.setup({
-			sources = {
-				null_ls.builtins.formatting.stylua,
-				null_ls.builtins.formatting.prettier,
-				null_ls.builtins.formatting.google_java_format,
-				null_ls.builtins.diagnostics.erb_lint,
-				null_ls.builtins.diagnostics.rubocop,
-				null_ls.builtins.formatting.rubocop,
-			},
-		})
-	end,
+  "nvimtools/none-ls.nvim",
+  dependencies = {
+    "nvim-lua/plenary.nvim",
+  },
+  event = { "BufReadPost", "BufNewFile" },
+  config = function()
+    local null_ls = require("null-ls")
+    local formatting = null_ls.builtins.formatting
+    local diagnostics = null_ls.builtins.diagnostics
+
+    -- List of desired sources (without availability checks)
+    local desired_sources = {
+      formatting.stylua,
+      formatting.prettier.with({
+        extra_args = { "--single-quote", "--jsx-single-quote" },
+      }),
+      formatting.google_java_format,
+      diagnostics.erb_lint,
+      diagnostics.rubocop,
+      formatting.rubocop,
+    }
+
+    -- Filter sources based on executable availability
+    local sources = {}
+    for _, source in ipairs(desired_sources) do
+      local command = source._opts and source._opts.command or source.command
+      if vim.fn.executable(command) == 1 then
+        table.insert(sources, source)
+      else
+        vim.notify_once("[null-ls] Not found: " .. command, vim.log.levels.WARN)
+      end
+    end
+
+    null_ls.setup({
+      debug = false,
+      sources = sources,
+      on_attach = function(client)
+        if client.server_capabilities.documentFormattingProvider then
+          vim.keymap.set("n", "<leader>cf", vim.lsp.buf.format, { desc = "[C]ode [F]ormat" })
+        end
+      end,
+    })
+  end,
 }
--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,31 +1,49 @@
 return {
-	{
-		"nvim-telescope/telescope-ui-select.nvim",
-	},
-	{
-		"nvim-telescope/telescope.nvim",
-		tag = "0.1.5",
-		dependencies = { "nvim-lua/plenary.nvim" },
-		config = function()
-			require("telescope").setup({
-				extensions = {
-					["ui-select"] = {
-						require("telescope.themes").get_dropdown({}),
-					},
-				},
-				defaults = {
-					file_ignore_patterns = {
-						".git\\",
-						"target/*",
-					},
-				},
-			})
-			local builtin = require("telescope.builtin")
-			vim.keymap.set("n", "<C-o>", builtin.find_files, {})
-			vim.keymap.set("n", "<C-p>", builtin.commands, {})
-			vim.keymap.set("n", "<C-i>", builtin.live_grep, {})
+  {
+    "nvim-telescope/telescope.nvim",
+    tag = "0.1.5",
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      "nvim-telescope/telescope-ui-select.nvim",
+    },
+    config = function()
+      require("telescope").setup({
+        defaults = {
+          file_ignore_patterns = {
+            "^.git/",        -- Improved git ignore pattern
+            "^target/",       -- Rust/Java builds
+            "%.class$",       -- Java classes
+          },
+          mappings = {
+            i = {
+              ["<C-j>"] = require("telescope.actions").move_selection_next,
+              ["<C-k>"] = require("telescope.actions").move_selection_previous,
+              ["<Esc>"] = require("telescope.actions").close,
+            },
+          },
+          layout_strategy = "horizontal",
+          winblend = 10,      -- Slight transparency
+        },
+        extensions = {
+          ["ui-select"] = {
+            require("telescope.themes").get_dropdown({
+              layout_config = {
+                width = 0.8,
+                height = 0.7,
+              },
+            }),
+          },
+        },
+      })
 
-			require("telescope").load_extension("ui-select")
-		end,
-	},
+      -- Load UI-select extension
+      require("telescope").load_extension("ui-select")
+
+      -- Your original keymaps (unchanged)
+      local builtin = require("telescope.builtin")
+      vim.keymap.set("n", "<C-o>", builtin.find_files, { desc = "Find Files" })
+      vim.keymap.set("n", "<C-p>", builtin.commands, { desc = "Commands" })
+      vim.keymap.set("n", "<C-i>", builtin.live_grep, { desc = "Live Grep" })
+    end,
+  },
 }
